010678854cefb0d8484be3aabe92150cb2f5b923
Sean <stank3391@conestogac.on.ca>
2025-04-11 16:40:39 -0400

    Implemented API for jobs. (Get and POST). Cleaned up some route names for the appointment API

---
+++ EnterpriseProj/Messages/JobInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace EnterpriseProj.Messages
+{
+	public class JobInfo
+	{
+		public int JobId { get; set; }
+		public string? JobName { get; set; }
+	}
+}
---
+++ EnterpriseProj/Messages/NewJob.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnterpriseProj.Messages
+{
+	public class NewJob
+	{
+		[Required(ErrorMessage = "Job name is required.")]
+		public string JobName { get; set; }
+	}
+}
