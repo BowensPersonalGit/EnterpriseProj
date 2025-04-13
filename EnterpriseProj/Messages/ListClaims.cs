c66f84180b92ef0bf53bfa00a973e39c0306672c
jschulz8138 <115745519+jschulz8138@users.noreply.github.com>
2025-04-12 21:13:00 -0400

    I have implemented the billing and appointment APIs (#5)
    Co-authored-by: Sean <stank3391@conestogac.on.ca>

---
+++ EnterpriseProj/Messages/ListClaims.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EnterpriseProj.Messages
+{
+	public class ListClaims
+	{
+		public List<ClaimInfo> Claims { get; set; } = new List<ClaimInfo>();
+	}
+}
