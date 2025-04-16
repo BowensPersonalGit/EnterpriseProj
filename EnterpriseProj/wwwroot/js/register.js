8551cf4465463464ac8b9f8baf739c5124bf19de
Bowen Sittler <115740150+BowensPersonalGit@users.noreply.github.com>
2025-04-15 20:13:59 -0400

    cleaned js and css in login and register views. (#12)
    GGEZ

---
+++ EnterpriseProj/wwwroot/js/register.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const roleSelect = document.getElementById('Role');
+    const jobContainer = document.getElementById('jobContainer');
+
+    function toggleJobDropdown() {
+        const selectedRole = roleSelect.options[roleSelect.selectedIndex].text.toLowerCase();
+        if (selectedRole === 'practitioner') {
+            jobContainer.style.display = 'block';
+        } else {
+            jobContainer.style.display = 'none';
+        }
+    }
+
+    roleSelect.addEventListener('change', toggleJobDropdown);
+    toggleJobDropdown();
+});
